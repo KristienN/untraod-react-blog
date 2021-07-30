1b112675621927d3cb8283346f684a6be0bd490e
KristienN <62649348+KristienN@users.noreply.github.com>
2021-07-30 21:50:03 +0200

    tailwind config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container sticky top-0 fixed bg-blue-500 text-center text-white rounded-full sm:max-w-full hover:bg-blue-800 p-5">
+      App Components
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="mb-20">
+            <h1>Hello, Welcome to my Blog!</h1>
+            <p></p>
+        </div>
+    )
+}
+
+export default Home
